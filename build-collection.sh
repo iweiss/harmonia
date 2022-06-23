fb639b40e730fff7f37d1558e87b5986692b0e9d
Romain Pelisse <belaran@gmail.com>
2022-06-23 14:22:25 +0200

    fixup

--- build-collection.sh
+++ build-collection.sh
@@ -62,7 +62,6 @@ if [ -n "${VERSION}" ]; then
 fi
 
 echo "Replace dependency to ${DOWNSTREAM_NS}.${JBOSS_UPSTREAM_NAME} by ${DOWNSTREAM_NS}.${JBOSS_DOWNSTREAM_NAME} (if any)."
-set +e
 grep -e "${DOWNSTREAM_NS}.${JBOSS_UPSTREAM_NAME}" -r . | cut -f1 -d: | \
 while
   read -r file_to_edit
@@ -71,7 +70,6 @@ do
   sed -i "${file_to_edit}" -e "s;${DOWNSTREAM_NS}.${JBOSS_UPSTREAM_NAME};${DOWNSTREAM_NS}.${JBOSS_DOWNSTREAM_NAME};"
   echo 'Done.'
 done
-set -e
 
 echo 'Display changes performed on code base:'
 git --no-pager diff --no-color -w .
