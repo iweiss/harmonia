ea93b8e1c304636f18750ebc84be18a25f818f5d
Romain Pelisse <belaran@gmail.com>
2022-08-29 19:30:07 +0200

    molecule-downstream: add support to use collection instead of copy

--- molecule-downstream.sh
+++ molecule-downstream.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -eo pipefail
 
+
+readonly PATH_TO_MOLECULE_REQUIREMENTS_FILE=${PATH_TO_MOLECULE_REQUIREMENTS_FILE:-'molecule/requirements.yml'}
+readonly PATH_TO_REQUIREMENTS_TEMPLATE=${PATH_TO_REQUIREMENTS_TEMPLATE:-'molecule/.ci_requirements.yml.j2'}
+
+generateRequirementsFromCItemplateIfProvided() {
+  local path_to_collection=${1}
+  local path_to_template=${2}
+  local path_to_requirements_file=${3}
+
+  if [ ! -e "${path_to_template}" ]; then
+    echo "Path to template to generate requirements.yml is invalid: ${path_to_template}, aborting."
+    exit 4
+  fi
+
+  if [ -d "${path_to_collection}" ]; then
+    ansible -m template \
+            -a "src=${path_to_template} dest=${path_to_requirements_file}" \
+            -e path_to_collection="${path_to_collection}"
+            localhost
+  else
+    echo "Invalid path to collection (does not exists or not a directory): ${path_to_collection}."
+    exit 5
+  fi
+}
+
 copyCollectionFrom() {
   local path_to_collection=${1}
   local workdir=${WORKDIR:-"${2}"}
@@ -37,7 +62,16 @@ if [ -n "${LAST_SUCCESS_FULL_BUILD_ID}" ]; then
      echo "PARENT_JOB_HOME not provided and required for this kind of build, aborting..."
      exit 3
   fi
-  copyCollectionFrom "${PARENT_JOB_HOME}/builds/${LAST_SUCCESS_FULL_BUILD_ID}/archive/workdir/downstream"/*
+  readonly PATH_TO_COLLECTION="${PARENT_JOB_HOME}/builds/${LAST_SUCCESS_FULL_BUILD_ID}/archive/workdir/downstream"
+  if [[ "${JOB_NAME}" =~ .*"-dot".* ]]; then
+    echo "${JOB_NAME} will use the latest available collection to run the tests."
+    generateRequirementsFromCItemplateIfProvided "${PATH_TO_COLLECTION}" \
+                                                 "${PATH_TO_MOLECULE_REQUIREMENTS_FILE}" \
+                                                 "${PATH_TO_REQUIREMENTS_TEMPLATE}"
+  else
+    echo "${JOB_NAME} will copy over the collection ${PATH_TO_COLLECTION} and run its molecule tests."
+    copyCollectionFrom "${PATH_TO_COLLECTION}"/*
+  fi
 else
   echo "TODO: a git checkout from gitlab"
   exit 4
